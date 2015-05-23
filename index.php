440846037051775bce0889e1a5cfa9b34098ad77
ChelseaStats <ChelseaStats@users.noreply.github.com>
2015-05-23 15:30:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,14 +9,12 @@
  *  Match : 60 seconds * 165 minutes ( 45 [1H] + 15 + 45 [2H] + 5 + 15 [1H ET] + 5 + 15 [2H ET] + 5 + 15 [pens])
  *          - the rest of the time slots are HT, FT, HT ET and pre-pens. comfort breaks.
  * 
- *  The bot class is ingnored from this repo as it contains twitter API access that is private.
- *  The class is no more than 20 lines of code and can be available on request (redacted).
- * 
  *  Pull requests to this file will be ignored.
  */
 	$match = 60*165; 
 	ini_set('max_execution_time', $match);
-	require_once ('twitteroauth.php');
-	require_once( 'tweeter.php' );
+	require_once( 'config.php');
+	require_once( 'twitteroauth.php');
+	require_once( 'class.bot.php' );
 	$bot = new bot();
 	$bot->Process('commentary-log.csv');
