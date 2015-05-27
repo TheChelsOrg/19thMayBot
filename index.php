30ad77f1965e307b6ee9ffe66103e5809528994e
ChelseaStats <ChelseaStats@users.noreply.github.com>
2015-05-27 13:24:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
  */
 	$match = 60*165; 
 	ini_set('max_execution_time', $match);
-	require_once( 'config.php');
+	require_once( '../config.php');
 	require_once( 'twitteroauth.php');
 	require_once( 'class.bot.php' );
 	$bot = new bot();
