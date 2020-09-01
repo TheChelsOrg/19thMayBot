93a87a5460a01cbb1be98b7d230fad363ca5bf4e
MatBenfield <MatBenfield@users.noreply.github.com>
2020-09-01 21:18:44 +0100

    pylinting
    tidyup and errors only

--- python/send_tweet.py
+++ python/send_tweet.py
@@ -1,19 +1,23 @@
  # importing the module
-import tweepy
+"""
+ Build and Tweet
+"""
+
 import os
 import csv
 import time
+import tweepy
 
 # personal details 
 consumer_key = os.getenv('c_key')
 consumer_secret = os.getenv('c_secret')
 access_token = os.getenv('a_token')
 access_token_secret = os.getenv('a_secret')
-# authentication of consumer key and secret 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
-# authentication of access token and secret 
-auth.set_access_token(access_token, access_token_secret) 
-api = tweepy.API(auth) 
+# authentication of consumer key and secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# authentication of access token and secret
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
 def get_offset(i):
     return (int(i) * 60)
@@ -22,10 +26,5 @@ with open('commentary-log.csv') as csvfile:
  reader = csv.DictReader(csvfile)
  for row in reader:
     time.sleep(get_offset(row['offset']))
-    # update the status 
-    api.update_status(status = row['text']) 
-
-
-
-
-
+    # update the status
+    api.update_status(status = row['text'])
